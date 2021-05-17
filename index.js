ab4bbc9caa6312d1870237e3c0fc2785327d3aa9
bin <vincent418@icloud.com>
2021-05-17 15:17:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-document.querySelector.remove('main')
+document.querySelector("main").remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "USATnpmeam is the champion!";
+newHeader.className = "victory"
